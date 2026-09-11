6572ce862b1491e230a07ba6fb10f1fd1e198360
Alberto Degioanni <68069909+degios@users.noreply.github.com>
2026-09-11 11:17:59 +0200

    Update madex.js

--- scripts/madex.js
+++ scripts/madex.js
@@ -47,6 +47,7 @@ window.MXLib=function(pServiceWorkerVersion){
                 }
               });
             }
+            console.log('MXLib: version ' + reg.active.scriptURL)
             if (!reg.active && (reg.installing || reg.waiting)) {
               // No active web worker and we have installed or are installing
               // one for the first time. Simply wait for it to activate.
