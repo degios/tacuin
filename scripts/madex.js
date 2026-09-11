507d9e9cf077c2531dac49258507f08d126d29b1
Alberto Degioanni <68069909+degios@users.noreply.github.com>
2026-09-11 10:27:37 +0200

    Update madex.js

--- scripts/madex.js
+++ scripts/madex.js
@@ -35,14 +35,14 @@ window.MXLib=function(pServiceWorkerVersion){
         // Wait for registration to finish before dropping the <script> tag.
         // Otherwise, the browser will load the script multiple times,
         // potentially different versions.
-        var serviceWorkerUrl = 'sw.js?v=' + this.swVersion;
+        var serviceWorkerUrl = 'sw.js?v=' + MXLib.swVersion;
         navigator.serviceWorker.register(serviceWorkerUrl)
           .then((reg) => {
             function waitForActivation(serviceWorker) {
               serviceWorker.addEventListener('statechange', () => {
                 if (serviceWorker.state == 'activated') {
                   console.log('Installed new service worker.');
-                  this._load();
+                  MXLib._load();
                 }
               });
             }
@@ -50,7 +50,7 @@ window.MXLib=function(pServiceWorkerVersion){
               // No active web worker and we have installed or are installing
               // one for the first time. Simply wait for it to activate.
               waitForActivation(reg.installing || reg.waiting);
-            } else if (!reg.active.scriptURL.endsWith(this.swVersion)) {
+            } else if (!reg.active.scriptURL.endsWith(MXLib.swVersion)) {
               // When the app updates the serviceWorkerVersion changes, so we
               // need to ask the service worker to update.
               console.log('New service worker available.');
@@ -59,7 +59,7 @@ window.MXLib=function(pServiceWorkerVersion){
             } else {
               // Existing service worker is still good.
               console.log('Loading app from service worker.');
-              this._load();
+              MXLib._load();
             }
           });
 
@@ -70,7 +70,7 @@ window.MXLib=function(pServiceWorkerVersion){
             console.warn(
               'Failed to load app from service worker. Falling back to plain <script> tag.',
             );
-            this._load();
+            MXLib._load();
           }
         }, 4000);
       });
